2eeb4664ebe9e4e445b0f3c9ded686d0b110731e
Jeong Lee <jsl133@ucsd.edu>
2018-02-06 00:55:29 -0800

    first

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+  cognito: {
+    USER_POOL_ID: "us-east-1_T8EA8Fh3e",
+    APP_CLIENT_ID: "7b0uktqlmob26fk59omh8a1u0v"
+  }
+};
---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Dogs</h1>
+          <p>Dogs everywhere</p>
+        </div>
+      </div>
+    );
+  }
+}
